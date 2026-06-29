688b32214f4f4c75684b37215599e7bc2b803fd9
Michdo93 <michaeldoerflinger93@gmail.com>
2026-06-29 19:05:56 +0200

    fixed relative path

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -5,56 +5,71 @@ using UnityEngine.UI;
 using SimpleJSON;
 
 public class GUIManager : MonoBehaviour {
-	//public static GUIManager instance;
-	public JSONNode strings;
-	public Dropdown language;
-	public Text label;
+    //public static GUIManager instance;
+    public JSONNode strings;
+    public Dropdown language;
+    public Text label;
 
-	private bool dataLoaded = false;
-	private string nameStrings = "./ExternalData/strings.json";
+    private bool dataLoaded = false;
+    // Hier nur den reinen Ordner- und Dateinamen angeben
+    private string nameStrings = "ExternalData/strings.json";
 
-	public void Awake() {
-		StaticClass.instance = this;
-	}
+    public void Awake() {
+        StaticClass.instance = this;
+    }
 
-	private void Start() {
-		if (StaticClass.selectedLanguage == null) {
-			StaticClass.selectedLanguage = "English";
-		} else {
-			strings = StaticClass.strings;
-			GUIController.changeMenu ();
-		}
+    private void Start() {
+        if (StaticClass.selectedLanguage == null) {
+            StaticClass.selectedLanguage = "English";
+        } else {
+            strings = StaticClass.strings;
+            GUIController.changeMenu ();
+        }
 
-		label.text = StaticClass.selectedLanguage;
-		StartCoroutine (loadData());
-	}
+        label.text = StaticClass.selectedLanguage;
+        StartCoroutine (loadData());
+    }
 
-	IEnumerator loadData() {
-		string prefix = "";
-		string pathname;
-		string iniData;
+    IEnumerator loadData() {
+        string pathname;
+        string iniData;
 
-		if (!Application.isWebPlayer)
-			prefix = "file://";
+        // Unterscheidung zwischen WebGL-Browser und lokalem Unity Editor/PC-Build
+#if UNITY_WEBGL && !UNITY_EDITOR
+        // Application.absoluteURL liefert z.B. "https://michdo93.github.io/SuperCube/"
+        // Wir hängen einfach den Pfad zur JSON-Datei an.
+        pathname = Application.absoluteURL + nameStrings;
+#else
+        // Für den lokalen Unity Editor: Nutzt den korrekten lokalen file:// Pfad
+        pathname = "file://" + Application.dataPath + "/../" + nameStrings;
+#endif
 
-		pathname = prefix + Application.dataPath + "/.." + nameStrings;
-		WWW www = new WWW (pathname);
-		yield return www;
+        // Debug-Ausgabe in der Browser-Konsole, damit du genau siehst, was geladen wird
+        Debug.Log("Lade Strings von Pfad: " + pathname);
 
-		iniData = www.text;
-		strings = JSON.Parse (iniData) as JSONNode;
-		dataLoaded = true;
+        WWW www = new WWW (pathname);
+        yield return www;
 
-		if(dataLoaded) {
-			for (int i = 0; i < strings.Count; i++) {
-				language.options.Add (new Dropdown.OptionData (strings [i] ["type"].Value));
-			}
+        // Fehler abfangen, falls beim Laden etwas schiefgeht
+        if (!string.IsNullOrEmpty(www.error)) {
+            Debug.LogError("Fehler beim Laden der JSON: " + www.error);
+            yield break;
+        }
 
-			language.onValueChanged.AddListener(delegate {
-				StaticClass.selectedLanguage = strings[language.value]["type"];
-				GUIController.changeMenu();
-			});
-			StaticClass.strings = strings;
-		}
-	}
+        iniData = www.text;
+        strings = JSON.Parse (iniData) as JSONNode;
+        dataLoaded = true;
+
+        if(dataLoaded && strings != null) {
+            for (int i = 0; i < strings.Count; i++) {
+                language.options.Add (new Dropdown.OptionData (strings [i] ["type"].Value));
+            }
+
+            language.onValueChanged.AddListener(delegate {
+                StaticClass.selectedLanguage = strings[language.value]["type"];
+                GUIController.changeMenu();
+            });
+            StaticClass.strings = strings;
+        }
+    }
 }
